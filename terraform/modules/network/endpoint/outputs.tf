9653bac63d70f4cb42d53a282f9431bc098f7488
wo969776-cloud <wo969776@gmail.com>
2026-05-05 23:24:15 +0900

    feat: add SSM Session Manager VPC endpoints (ec2, ssmmessages, ec2messages)

--- terraform/modules/network/endpoint/outputs.tf
+++ terraform/modules/network/endpoint/outputs.tf
@@ -12,3 +12,18 @@ output "ecr_dkr_endpoint_id" {
   description = "ECR DKR VPC Endpoint ID"
   value       = aws_vpc_endpoint.ecr_dkr.id
 }
+
+output "ec2_endpoint_id" {
+  description = "EC2 VPC Endpoint ID"
+  value       = aws_vpc_endpoint.ec2.id
+}
+
+output "ssmmessages_endpoint_id" {
+  description = "SSM Messages VPC Endpoint ID"
+  value       = aws_vpc_endpoint.ssmmessages.id
+}
+
+output "ec2messages_endpoint_id" {
+  description = "EC2 Messages VPC Endpoint ID"
+  value       = aws_vpc_endpoint.ec2messages.id
+}
