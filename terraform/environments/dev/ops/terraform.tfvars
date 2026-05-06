96ad0b1e7dab8b79fb39bfeb33cc62b2a002fe9f
2sso <201sohyun@naver.com>
2026-05-06 13:15:26 +0900

    fix:irsa arn 참조방식 변경

---
+++ terraform/environments/dev/ops/terraform.tfvars
@@ -0,0 +1,4 @@
+enable_observability_iam = true
+enable_fluentbit_irsa = true
+enable_prometheus_irsa = true
+enable_grafana_irsa = true
--- terraform/modules/ops/observability-iam/prometheus-irsa.tf
+++ terraform/modules/ops/observability-iam/prometheus-irsa.tf
@@ -1,6 +1,6 @@
 module "prometheus_irsa" {
   count  = var.enable_prometheus_irsa ? 1 : 0
-  source = "../../../../modules/api-service/eks-irsa"
+  source = "../../../modules/api-service/eks-irsa"
 
   role_name            = "${local.name_prefix}-prometheus-irsa"
   oidc_provider_arn    = var.eks_oidc_provider_arn
@@ -8,7 +8,7 @@ module "prometheus_irsa" {
   namespace            = var.prometheus_namespace
   service_account_name = var.prometheus_service_account_name
 
-  managed_policy_arns = [
-    aws_iam_policy.cloudwatch_read.arn
-  ]
+  managed_policy_arns = {
+    cloudwatch_read = aws_iam_policy.cloudwatch_read.arn
+  }
 }
